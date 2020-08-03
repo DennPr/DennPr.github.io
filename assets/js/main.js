f1bd894635eb0b893eda096612e0e8a7bdc26845
DennPr <64957381+DennPr@users.noreply.github.com>
2020-08-03 19:30:43 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -217,4 +217,10 @@
     aos_init();
   });
 
+
+  function resizeIframe(obj){
+     obj.style.height = 0;
+     obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+  }
+
 })(jQuery);
